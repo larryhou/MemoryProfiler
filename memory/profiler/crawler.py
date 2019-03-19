86a1231b3fa9b99b07370ce755ab6f7c4ad6b36d
larryhou <larryhou@foxmail.com>
2019-03-19 13:11:47 +0800

    Crawling snapshot

--- memory/profiler/crawler.py
+++ memory/profiler/crawler.py
@@ -7,19 +7,23 @@ import enum
 class ConnectionKind(enum.Enum):
     none, handle, native, managed, static = range(5)
 
+
 class ManagedStaticField(object):
-    def __init__(self, type_index:int = -1, field_type_index:int = -1, type_field_index:int = -1, static_field_index:int = -1):
-        self.type_index:int = type_index
-        self.type_field_index:int = type_field_index
-        self.static_field_index:int = static_field_index
-        self.field_type_index:int = field_type_index
+    def __init__(self, type_index: int = -1, field_type_index: int = -1, type_field_index: int = -1,
+                 static_field_index: int = -1):
+        self.type_index: int = type_index
+        self.type_field_index: int = type_field_index
+        self.static_field_index: int = static_field_index
+        self.field_type_index: int = field_type_index
+
 
 class ManagedConnection(object):
-    def __init__(self, from_ = 0, from_kind = ConnectionKind.none, to = 0, to_kind = ConnectionKind.none):
+    def __init__(self, from_=0, from_kind=ConnectionKind.none, to=0, to_kind=ConnectionKind.none):
         self.from_: int = from_
         self.to: int = to
         self.from_kind: ConnectionKind = from_kind
         self.to_kind: ConnectionKind = to_kind
+
 
 class ManagedObject(object):
     def __init__(self):
@@ -27,16 +31,16 @@ class ManagedObject(object):
         self.memory: bytes = None
         self.type_index: int = -1
         self.native_object_index: int = -1
-        self.manage_object_index: int = -1
+        self.managed_object_index: int = -1
         self.handle_index: int = -1
         self.size: int = 0
-
+        self.static_bytes:bytes = b''
 
 class MemorySnapshotCrawler(object):
     def __init__(self, snapshot: PackedMemorySnapshot):
         self.managed_objects: List[ManagedObject] = []
 
-        self.crawled: List[ManagedObject] = []
+        self.crawled_objects: List[ManagedObject] = []
         self.static_fields: List[ManagedStaticField] = []
         self.visit: Dict[int, int] = {}
         self.snapshot: PackedMemorySnapshot = snapshot
@@ -51,6 +55,7 @@ class MemorySnapshotCrawler(object):
 
         # connections
         self.managed_connections: List[ManagedConnection] = []
+        self.mono_script_connections: List[ManagedConnection] = []
         self.connections_from: Dict[int, List[ManagedConnection]] = []
         self.connections_to: Dict[int, List[ManagedConnection]] = []
 
@@ -61,19 +66,36 @@ class MemorySnapshotCrawler(object):
         self.handle_address_map: Dict[int, int] = {}
 
     def crawl(self):
-        self.preprocess_managed_types()
-        self.create_managed_connections()
+        self.init_managed_types()
+        self.init_managed_connections()
+        self.init_mono_script_connections()
+        self.init_managed_fields()
         self.crawl_hanles()
         self.crawl_static()
         self.crawl_managed_objects()
 
-    def preprocess_managed_types(self):
+    def init_managed_fields(self):
+        managed_types = self.snapshot.typeDescriptions
+        for mt in managed_types:
+            for field in mt.fields:
+                offset = field.name.rfind('>k__BackingField')
+                if offset >= 0:
+                    field.name = field.name[1:offset]
+
+    def init_mono_script_connections(self):
+        for n in range(len(self.managed_connections)):
+            mc = self.managed_connections[n]
+            if mc.to_kind == ConnectionKind.native \
+                    and self.snapshot.nativeObjects[mc.to].nativeTypeArrayIndex == self.nt_index.MonoScript:
+                self.mono_script_connections.append(mc)
+
+    def init_managed_types(self):
         managed_types = self.snapshot.typeDescriptions
         for n in range(len(managed_types)):
             mt = managed_types[n]
             mt.isUnityEngineObjectType = self.is_subclass_of_managed_type(mt, self.mt_index.unityengine_Object)
 
-    def create_managed_connections(self, exclude_native: bool = False):
+    def init_managed_connections(self, exclude_native: bool = False):
         managed_start = 0
         managed_stop = managed_start + len(self.snapshot.gcHandles)
         native_start = managed_start + managed_stop
@@ -91,6 +113,7 @@ class MemorySnapshotCrawler(object):
                 mc.to_kind = ConnectionKind.native
             if exclude_native and mc.from_kind == ConnectionKind.native: continue
             managed_connections.append(mc)
+            self.add_connection(from_=mc.from_, from_kind=mc.from_kind, to=mc.to, to_kind=mc.to_kind)
         self.managed_connections = managed_connections
 
     def add_connection(self, from_: int, from_kind: ConnectionKind, to: int, to_kind: ConnectionKind):
@@ -131,7 +154,7 @@ class MemorySnapshotCrawler(object):
         type_index = self.type_address_map.get(type_info_address)
         return -1 if type_index is None else type_index
 
-    def find_managed_object_of_native_object(self, native_address:int):
+    def find_managed_object_of_native_object(self, native_address: int):
         if native_address == 0: return -1
         if not self.managed_native_address_map:
             for n in range(len(self.managed_objects)):
@@ -160,7 +183,7 @@ class MemorySnapshotCrawler(object):
         object_index = self.native_object_address_map.get(address)
         return -1 if object_index is None else object_index
 
-    def find_handle_with_target_address(self, address:int):
+    def find_handle_with_target_address(self, address: int):
         if address == 0: return -1
         if not self.handle_address_map:
             for n in range(len(self.snapshot.gcHandles)):
@@ -169,17 +192,17 @@ class MemorySnapshotCrawler(object):
         handle_index = self.handle_address_map.get(address)
         return -1 if handle_index is None else handle_index
 
-    def get_connections_of(self, kind:ConnectionKind, index:int)->List[ManagedConnection]:
+    def get_connections_of(self, kind: ConnectionKind, index: int) -> List[ManagedConnection]:
         key = self.get_connection_key(kind=kind, index=index)
         references = self.connections_from.get(key)
         return references if references else []
 
-    def get_connections_referenced_by(self, kind:ConnectionKind, index:int)->List[ManagedConnection]:
+    def get_connections_referenced_by(self, kind: ConnectionKind, index: int) -> List[ManagedConnection]:
         key = self.get_connection_key(kind=kind, index=index)
         references = self.connections_to.get(key)
         return references if references else []
 
-    def get_connections_in_heap_section(self, section:MemorySection)->List[ManagedConnection]:
+    def get_connections_in_heap_section(self, section: MemorySection) -> List[ManagedConnection]:
         if not section.bytes: return []
         start_address = section.startAddress
         stop_address = start_address + len(section.bytes)
@@ -190,7 +213,7 @@ class MemorySnapshotCrawler(object):
                 references.append(ManagedConnection(to=n, to_kind=ConnectionKind.managed))
         return references
 
-    def find_mono_script_type(self, native_index: int)->Tuple[str, int]:
+    def find_mono_script_type(self, native_index: int) -> Tuple[str, int]:
         key = self.get_connection_key(kind=ConnectionKind.native, index=native_index)
         reference_list = self.connections_from.get(key)
         if reference_list:
@@ -228,7 +251,7 @@ class MemorySnapshotCrawler(object):
             type = self.snapshot.typeDescriptions[type.baseOrElementTypeIndex]
         return False
 
-    def contains_reference(self, type_index:int):
+    def contains_reference(self, type_index: int):
         mt = self.snapshot.typeDescriptions[type_index]
         if not mt.isValueType: return True
         managed_types = self.snapshot.typeDescriptions
@@ -250,15 +273,15 @@ class MemorySnapshotCrawler(object):
         # connect native object and managed object
         native_type_index = self.snapshot.nativeObjects[native_object_index].nativeTypeArrayIndex
         managed_object.native_object_index = native_object_index
-        self.snapshot.nativeObjects[native_object_index].managedObjectArrayIndex = managed_object.manage_object_index
+        self.snapshot.nativeObjects[native_object_index].managedObjectArrayIndex = managed_object.managed_object_index
         # connect native type and managed type
         self.snapshot.typeDescriptions[managed_object.type_index].nativeTypeArrayIndex = native_type_index
         self.snapshot.nativeTypes[native_type_index].managedTypeArrayIndex = managed_object.type_index
 
-    def set_object_size(self, managed_object: ManagedObject, object_type: TypeDescription):
+    def set_object_size(self, managed_object: ManagedObject, type: TypeDescription):
         if managed_object.size == 0:
             managed_object.size = self.heap_reader.read_object_size(
-                address=managed_object.address, object_type=object_type
+                address=managed_object.address, type=type
             )
 
     def try_create_managed_object(self, address: int) -> int:
@@ -267,13 +290,13 @@ class MemorySnapshotCrawler(object):
         mo = ManagedObject()
         mo.address = address
         mo.type_index = type_index
-        mo.manage_object_index = len(self.managed_objects)
+        mo.managed_object_index = len(self.managed_objects)
         self.try_connect_with_native_object(managed_object=mo)
-        self.set_object_size(managed_object=mo, object_type=self.snapshot.typeDescriptions[type_index])
-        self.visit[mo.address] = mo.manage_object_index
+        self.set_object_size(managed_object=mo, type=self.snapshot.typeDescriptions[type_index])
+        self.visit[mo.address] = mo.managed_object_index
         self.managed_objects.append(mo)
-        self.crawled.append(mo)
-        return mo.manage_object_index
+        self.crawled_objects.append(mo)
+        return mo.managed_object_index
 
     def crawl_hanles(self):
         handle_list = self.snapshot.gcHandles
@@ -301,16 +324,88 @@ class MemorySnapshotCrawler(object):
                                           static_field_index=len(self.static_fields))
                 self.static_fields.append(item)
                 static_list.append(item.static_field_index)
-        static_reader = StaticHeapReader(snapshot=self.snapshot, memory=b'')
+        static_reader = StaticHeapReader(snapshot=self.snapshot)
         for n in range(len(static_list)):
             static_index = static_list[n]
             static_field = self.static_fields[static_index]
+            static_class = managed_types[static_field.type_index]
+            field = static_class.fields[static_field.type_field_index]
             field_type = managed_types[static_field.field_type_index]
-            static_reader.load(memory=field_type.staticFieldBytes)
+
+            if not static_class.staticFieldBytes: continue
+            static_reader.load(memory=static_class.staticFieldBytes)
             if field_type.isValueType:
-                pass
+                mo = ManagedObject()
+                mo.address = field.offset
+                mo.managed_object_index = -static_field.static_field_index
+                mo.type_index = field_type.typeIndex
+                mo.static_bytes = static_class.staticFieldBytes
+                self.set_object_size(managed_object=mo, type=field_type)
+                self.crawled_objects.append(mo)
+                continue
 
-
+            address = static_reader.read_pointer(address=field.offset)
+            if address not in self.visit:
+                mo = ManagedObject()
+                mo.address = address
+                mo.managed_object_index = len(self.managed_objects)
+                mo.type_index = self.find_type_of_address(address)
+                if mo.type_index == -1:
+                    mo.type_index = field_type.typeIndex
+                # crawl gcHandle
+                handle_index = self.find_handle_with_target_address(address)
+                if handle_index != -1:
+                    mo.handle_index = handle_index
+                    self.snapshot.gcHandles[handle_index].managedObjectArrayIndex = mo.managed_object_index
+                    self.add_connection(from_kind=ConnectionKind.handle, from_=handle_index,
+                                        to_kind=ConnectionKind.managed, to=mo.managed_object_index)
+                self.set_object_size(managed_object=mo, type=field_type)
+                self.try_connect_with_native_object(managed_object=mo)
+                self.managed_objects.append(mo)
+                self.visit[address] = mo.managed_object_index
+                self.crawled_objects.append(mo)
+                self.add_connection(from_kind=ConnectionKind.static, from_=static_field.static_field_index,
+                                    to_kind=ConnectionKind.managed, to=mo.managed_object_index)
 
     def crawl_managed_objects(self):
-        pass
+        managed_types = self.snapshot.typeDescriptions
+        managed_count = len(managed_types)
+        for mo in self.crawled_objects:
+            type_index = mo.type_index
+            while type_index != -1:
+                reader = self.heap_reader
+                if mo.static_bytes:
+                    reader = StaticHeapReader(snapshot=self.snapshot, memory=mo.static_bytes)
+                mt = managed_types[mo.type_index]
+                if mt.isArray:
+                    if mt.baseOrElementTypeIndex < 0 or mt.baseOrElementTypeIndex >= managed_count:
+                        break
+                    element_type = managed_types[mt.baseOrElementTypeIndex]
+                    # if element_type.isValueType
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- memory/profiler/heap.py
+++ memory/profiler/heap.py
@@ -2,8 +2,9 @@ from .core import PackedMemorySnapshot, TypeDescription
 from struct import unpack
 import operator
 
+
 class Quaternion(object):
-    def __init__(self, x = 0.0, y = 0.0, z = 0.0, w = 0.0):
+    def __init__(self, x=0.0, y=0.0, z=0.0, w=0.0):
         self.x = x
         self.y = y
         self.z = z
@@ -12,8 +13,9 @@ class Quaternion(object):
     def __repr__(self):
         return '[Quaternion] x={} y={} z={} w={}'.format(self.x, self.y, self.z, self.w)
 
+
 class Decimal(object):
-    def __init__(self, flags = 0, hi = 0, lo = 0, mid = 0):
+    def __init__(self, flags=0, hi=0, lo=0, mid=0):
         self.flags = flags
         self.hi = hi
         self.lo = lo
@@ -22,8 +24,9 @@ class Decimal(object):
     def __repr__(self):
         return '[Decimal] flags={} hi={} lo={} mid={}'.format(self.flags, self.hi, self.lo, self.mid)
 
+
 class HeapSegment(object):
-    def __init__(self, memory:bytes, offset:int, length:int):
+    def __init__(self, memory: bytes, offset: int, length: int):
         self.memory = memory
         self.offset = offset
         self.length = length
@@ -34,16 +37,17 @@ class HeapSegment(object):
             len(self.memory), self.offset, self.length, self.qualified
         )
 
+
 class HeapReader(object):
-    def __init__(self, snapshot:PackedMemorySnapshot):
+    def __init__(self, snapshot: PackedMemorySnapshot):
         self.snapshot = snapshot
         self.heap_sections = snapshot.managedHeapSections
 
-        self.memory:bytes = bytes()
-        self.start_address:int = -1
-        self.stop_address:int = -1
+        self.memory: bytes = bytes()
+        self.start_address: int = -1
+        self.stop_address: int = -1
 
-    def try_begin_read(self, address:int)->int:
+    def try_begin_read(self, address: int) -> int:
         if address == 0: return -1
         if self.start_address <= address < self.stop_address:
             return address - self.start_address
@@ -57,71 +61,71 @@ class HeapReader(object):
         self.memory = heap.bytes
         return address - self.start_address
 
-    def read_sbyte(self, address:int)->int:
+    def read_sbyte(self, address: int) -> int:
         offset = self.try_begin_read(address)
         if offset == -1: return 0
-        return unpack('b', self.memory[offset:offset+1])[0]
+        return unpack('b', self.memory[offset:offset + 1])[0]
 
-    def read_ubyte(self, address:int)->int:
+    def read_ubyte(self, address: int) -> int:
         offset = self.try_begin_read(address)
         if offset == -1: return 0
         return self.memory[offset]
 
-    def read_char(self, address:int)->str:
+    def read_char(self, address: int) -> str:
         offset = self.try_begin_read(address)
         if offset == -1: return ''
         return chr(self.memory[offset])
 
-    def read_boolean(self, address:int)->bool:
+    def read_boolean(self, address: int) -> bool:
         return self.read_ubyte(address) != 0
 
-    def read_uint8(self, address:int)->int:
+    def read_uint8(self, address: int) -> int:
         return self.read_ubyte(address)
 
-    def read_sint8(self, address:int)->int:
+    def read_sint8(self, address: int) -> int:
         return self.read_sbyte(address)
 
-    def read_uint16(self, address:int)->int:
+    def read_uint16(self, address: int) -> int:
         offset = self.try_begin_read(address)
         if offset == -1: return 0
-        return unpack('>H', self.memory[offset:offset+2])[0]
+        return unpack('>H', self.memory[offset:offset + 2])[0]
 
-    def read_sint16(self, address:int)->int:
+    def read_sint16(self, address: int) -> int:
         offset = self.try_begin_read(address)
         if offset == -1: return 0
-        return unpack('>h', self.memory[offset:offset+2])[0]
+        return unpack('>h', self.memory[offset:offset + 2])[0]
 
-    def read_uint32(self, address:int)->int:
+    def read_uint32(self, address: int) -> int:
         offset = self.try_begin_read(address)
         if offset == -1: return 0
-        return unpack('>I', self.memory[offset:offset+4])[0]
+        return unpack('>I', self.memory[offset:offset + 4])[0]
 
-    def read_sint32(self, address:int)->int:
+    def read_sint32(self, address: int) -> int:
         offset = self.try_begin_read(address)
         if offset == -1: return 0
-        return unpack('>i', self.memory[offset:offset+4])[0]
+        return unpack('>i', self.memory[offset:offset + 4])[0]
 
-    def read_uint64(self, address:int)->int:
+    def read_uint64(self, address: int) -> int:
         offset = self.try_begin_read(address)
         if offset == -1: return 0
-        return unpack('>Q', self.memory[offset:offset+8])[0]
+        return unpack('>Q', self.memory[offset:offset + 8])[0]
 
-    def read_sint64(self, address:int)->int:
+    def read_sint64(self, address: int) -> int:
         offset = self.try_begin_read(address)
         if offset == -1: return 0
-        return unpack('>q', self.memory[offset:offset+8])[0]
+        return unpack('>q', self.memory[offset:offset + 8])[0]
 
-    def read_single(self, address:int)->int:
+    def read_single(self, address: int) -> int:
         offset = self.try_begin_read(address)
         if offset == -1: return 0.0
-        return unpack('>f', self.memory[offset:offset+4])[0]
+        return unpack('>f', self.memory[offset:offset + 4])[0]
 
-    def read_double(self, address:int)->int:
+    def read_double(self, address: int) -> int:
         offset = self.try_begin_read(address)
         if offset == -1: return 0.0
-        return unpack('>d', self.memory[offset:offset+8])[0]
+        return unpack('>d', self.memory[offset:offset + 8])[0]
 
-    def read_decimal(self, address:int)->int:
+    def read_decimal(self, address: int) -> int:
         offset = self.try_begin_read(address)
         if offset == -1: return Decimal()
         return Decimal(
@@ -131,7 +135,7 @@ class HeapReader(object):
             mid=self.read_uint32(address + 12),
         )
 
-    def read_quaternion(self, address:int)->Quaternion:
+    def read_quaternion(self, address: int) -> Quaternion:
         offset = self.try_begin_read(address)
         if offset == -1: return Quaternion()
         type_size = 4
@@ -142,31 +146,31 @@ class HeapReader(object):
             w=self.read_single(address=address + type_size * 3)
         )
 
-    def read_matrix4x4(self, address:int):
+    def read_matrix4x4(self, address: int):
         offset = self.try_begin_read(address)
-        if offset == -1: return (0,)*16
+        if offset == -1: return (0,) * 16
         return tuple([self.read_single(address + 4 * n) for n in range(16)])
 
-    def read_pointer(self, address:int)->int:
+    def read_pointer(self, address: int) -> int:
         return self.read_uint64(address) if self.snapshot.vm.pointerSize == 8 else self.read_uint32(address)
 
-    def read_string(self, address:int)->str:
+    def read_string(self, address: int) -> str:
         offset = self.try_begin_read(address)
         if offset == -1: return ''
         length = self.read_sint32(address)
         if length <= 0:
             return 'length[{}] less then 0'.format(length) if length < 0 else ''
-        offset += 4 # string length
-        length *= 2 # wide char
-        return self.memory[offset:offset+length].decode(encoding='utf-16') # unicode encoding
+        offset += 4  # string length
+        length *= 2  # wide char
+        return self.memory[offset:offset + length].decode(encoding='utf-16')  # unicode encoding
 
-    def read_string_length(self, address:int)->int:
+    def read_string_length(self, address: int) -> int:
         offset = self.try_begin_read(address)
         if offset == -1: return 0
         length = self.read_sint32(address)
         return length if length > 0 else 0
 
-    def read_array_length(self, address:int, array_type:TypeDescription)->int:
+    def read_array_length(self, address: int, array_type: TypeDescription) -> int:
         vm = self.snapshot.vm
         bounds = self.read_pointer(address + vm.arrayBoundsOffsetInHeader)
         if bounds == 0:
@@ -176,7 +180,7 @@ class HeapReader(object):
             length *= self.read_pointer(bounds + n * vm.pointerSize)
         return length
 
-    def read_array_length_of_dimension(self, address:int, array_type:TypeDescription, dimension:int)->int:
+    def read_array_length_of_dimension(self, address: int, array_type: TypeDescription, dimension: int) -> int:
         if dimension >= array_type.arrayRank: return 0
         vm = self.snapshot.vm
         bounds = self.read_pointer(address + vm.arrayBoundsOffsetInHeader)
@@ -184,27 +188,27 @@ class HeapReader(object):
             return self.read_pointer(address + vm.arraySizeOffsetInHeader)
         return self.read_pointer(bounds + dimension * vm.pointerSize)
 
-    def read_object_size(self, address:int, object_type:TypeDescription)->int:
+    def read_object_size(self, address: int, type: TypeDescription) -> int:
         offset = self.try_begin_read(address)
         if offset == -1: return 0
-        if object_type.isArray:
-            if object_type.baseOrElementTypeIndex < 0 or object_type.baseOrElementTypeIndex >= len(self.snapshot.typeDescriptions):
-                print('[ERR] not invalid baseOrElementTypeIndex={0}'.format(object_type.baseOrElementTypeIndex))
+        if type.isArray:
+            if type.baseOrElementTypeIndex < 0 or type.baseOrElementTypeIndex >= len(self.snapshot.typeDescriptions):
+                print('[ERR] not invalid baseOrElementTypeIndex={0}'.format(type.baseOrElementTypeIndex))
                 return 0
-            array_length = self.read_array_length(address, array_type=object_type)
-            element_type = self.snapshot.typeDescriptions[object_type.baseOrElementTypeIndex]
+            array_length = self.read_array_length(address, array_type=type)
+            element_type = self.snapshot.typeDescriptions[type.baseOrElementTypeIndex]
             element_size = element_type.size if element_type.isValueType else self.snapshot.vm.pointerSize
             return self.snapshot.vm.arrayHeaderSize + element_size * array_length
-        if object_type.name == 'System.String':
+        if type.name == 'System.String':
             size = self.snapshot.vm.objectHeaderSize
-            size += 4 # string length
-            size += self.read_string_length(address + self.snapshot.vm.objectHeaderSize) * 2 # string data size
-            size += 2 # \x00\x00
+            size += 4  # string length
+            size += self.read_string_length(address + self.snapshot.vm.objectHeaderSize) * 2  # string data size
+            size += 2  # \x00\x00
             return size
-        return object_type.size
+        return type.size
 
-    def read_object_memory(self, address:int, object_type:TypeDescription)->HeapSegment:
-        size = self.read_object_size(address, object_type)
+    def read_object_memory(self, address: int, type: TypeDescription) -> HeapSegment:
+        size = self.read_object_size(address, type)
         if size <= 0:
             return HeapSegment(b'', 0, 0)
         offset = self.try_begin_read(address)
@@ -212,7 +216,7 @@ class HeapReader(object):
             return HeapSegment(b'', 0, 0)
         return HeapSegment(self.memory, offset, size)
 
-    def find_heap_of_address(self, address)->int:
+    def find_heap_of_address(self, address) -> int:
         _min, _max = 0, len(self.heap_sections) - 1
         while _min <= _max:
             _mid = (_max + _min) >> 1
@@ -225,21 +229,20 @@ class HeapReader(object):
                 return _mid
         return -1
 
+
 class StaticHeapReader(HeapReader):
 
-    def __init__(self, snapshot: PackedMemorySnapshot, memory: bytes):
+    def __init__(self, snapshot: PackedMemorySnapshot, memory: bytes = b''):
         super().__init__(snapshot)
         self.load(memory)
 
-    def load(self, memory:bytes):
+    def load(self, memory: bytes):
         self.memory = memory
         self.start_address = 0
         self.stop_address = len(memory)
 
-    def try_begin_read(self, address:int):
+    def try_begin_read(self, address: int):
         if address == 0 \
-            or len(self.memory) == 0 or address >= len(self.memory):
+                or len(self.memory) == 0 or address >= len(self.memory):
             return -1
         return address
-
-
