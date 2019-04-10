d5ea103af99d0af783b25a5a795db128be5a5fa0
larryhou <larryhou@tencent.com>
2019-04-10 09:55:08 +0800

    Add crawling cache

--- MemoryCrawler/MemoryCrawler/Crawler/cache.h
+++ MemoryCrawler/MemoryCrawler/Crawler/cache.h
@@ -19,6 +19,7 @@ class CrawlerCache
 {
     TimeSampler<std::nano> sampler;
     sqlite3 *database;
+    char buffer[32*1024];
     
 public:
     CrawlerCache();
@@ -37,6 +38,57 @@ private:
     void insert(InstanceManager<EntityConnection> &connections);
     void createObjectTable();
     void insert(InstanceManager<ManagedObject> &objects);
+    
+    template <typename T>
+    void insert(const char * sql, InstanceManager<T> &manager, std::function<void(T &item, sqlite3_stmt *stmt)> kernel);
+    
+    template <typename T>
+    void insert(const char * sql, Array<T> &array, std::function<void(T &item, sqlite3_stmt *stmt)> kernel);
+    
+    void create(const char *sql);
 };
+
+template <typename T>
+void CrawlerCache::insert(const char * sql, Array<T> &array, std::function<void(T &item, sqlite3_stmt *stmt)> kernel)
+{
+    char *errmsg;
+    sqlite3_stmt *stmt;
+    
+    sqlite3_exec(database, "BEGIN TRANSACTION", nullptr, nullptr, &errmsg);
+    sqlite3_prepare_v2(database, sql, (int)strlen(sql), &stmt, nullptr);
+    
+    for (auto i = 0; i < array.size; i++)
+    {
+        kernel(array[i], stmt);
+        
+        if (sqlite3_step(stmt) != SQLITE_DONE) {}
+        sqlite3_reset(stmt);
+    }
+    
+    sqlite3_exec(database, "COMMIT TRANSACTION", nullptr, nullptr, &errmsg);
+    sqlite3_finalize(stmt);
+}
+
+template <typename T>
+void CrawlerCache::insert(const char * sql, InstanceManager<T> &manager, std::function<void(T &item, sqlite3_stmt *stmt)> kernel)
+{
+    char *errmsg;
+    sqlite3_stmt *stmt;
+    
+    sqlite3_exec(database, "BEGIN TRANSACTION", nullptr, nullptr, &errmsg);
+    sqlite3_prepare_v2(database, sql, (int)strlen(sql), &stmt, nullptr);
+    
+    for (auto i = 0; i < manager.size(); i++)
+    {
+        kernel(manager[i], stmt);
+        
+        if (sqlite3_step(stmt) != SQLITE_DONE) {}
+        sqlite3_reset(stmt);
+    }
+    
+    sqlite3_exec(database, "COMMIT TRANSACTION", nullptr, nullptr, &errmsg);
+    sqlite3_finalize(stmt);
+}
+
 
 #endif /* cache_h */
